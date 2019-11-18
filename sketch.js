6c2fd3fde3cda013828b8193323ba5f026228b47
Git4JEOL <claener@naver.com>
2019-11-18 15:39:28 +0900

    Update sketch.js
    player can eat food now

--- sketch.js
+++ sketch.js
@@ -4,12 +4,21 @@ function setup() {
   player=createSprite(0,0,50,50);
   box1=createSprite(200,100,200,300);
   box2=createSprite(600,300,200,300);
-  collect = new group();
+  food = new Group();
+  //food
    for (var i = 0; i < 10; i++) {
-     var prey =createSprite(random(0,width),randim(0,height));
-     collect.add(prey);
+     var prey =createSprite(random(0,width),random(0,height));
+     food.add(prey);
    }
+   food.draw = function() { ellipse(0,0,10,10) }
+   //obstacles = new group();
+   /*for (var i = 0; i < 5; i++) {
+     var prey =createSprite(random(0,width),randim(0,height));
+     block.add(prey);
+   }
+   */
 }
+
 
 function draw() {
   background(100,200,300);
@@ -22,16 +31,21 @@ function draw() {
     b=random(0,255);
     player.shapeColor = color(r,g,b);//player color change random
   }
-  if(player.overlap(box1)||player.overlap(box2)){//overlap  check
+  player.overlap(food,eat);
+  if(player.overlap(box1)||player.overlap(box2)){//overlap
+    player.remove();//remove if overlap
+    food.remove();
+    box2.remove();
     fill(255);
     rect(0,0,800,400);//game over white background
-    player.remove();//remove if overlap
-    box1.remove();
-    box2.remove();
     textSize(50);
     fill(100,200,300);
     text('Game over', 300, 200);
     noLoop();//stop game
   }
   drawSprites();
+}
+function eat(player,food)
+{
+  food.remove();
 }
