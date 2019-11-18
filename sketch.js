7618be0de30055a4a52a7cfa62713dd477e65a0a
Git4JEOL <claener@naver.com>
2019-11-18 16:34:47 +0900

    Update sketch.js
    gonna make snake game

--- sketch.js
+++ sketch.js
@@ -1,14 +1,27 @@
+var food;
 var r,g,b; //random value
+var score=0; //score
 function setup() {
   createCanvas(800, 400);
   player=createSprite(0,0,50,50);
-  box1=createSprite(200,100,200,300);
-  box2=createSprite(600,300,200,300);
+  food = new Group();
+  //food
+   for (var i = 0; i < 10; i++) {
+     var prey =createSprite(random(0,width),random(0,height),60,60);
+     prey.shapeColor= color(0,100,100);
+     food.add(prey);
+   }
+   obstacles = new Group();
+   for (var i = 0; i < 10; i++) {
+     var block =createSprite(random(0,width),random(0,height),random(20,50),random(20,50));
+     block.shapeColor= color(255);
+     obstacles.add(block);
+   }
+
 }
 
 function draw() {
-  background(100,200,300);
-  fill(100,200,300);
+  background(50);
   if(mouseIsPressed){ //move while mouseIsPressed
     player.position.x=mouseX;
     player.position.y=mouseY;
@@ -17,16 +30,26 @@ function draw() {
     b=random(0,255);
     player.shapeColor = color(r,g,b);//player color change random
   }
-  if(player.overlap(box1)||player.overlap(box2)){//overlap  check
-    fill(255);
-    rect(0,0,800,400);//game over white background
+    player.overlap(food,eat);
+  if(player.overlap(obstacles)){//overlap
     player.remove();//remove if overlap
-    box1.remove();
+    food.remove();
     box2.remove();
-    textSize(50);
-    fill(100,200,300);
-    text('Game over', 300, 200);
+    fill(0);
+    rect(0,0,800,400);//game over white background
+    noStroke();
+    textAlign(CENTER, CENTER);
+     textSize(72);
+    fill(255);
+    text('Game over', width/2,height/2);
     noLoop();//stop game
   }
   drawSprites();
+
+}
+
+function eat(player,food)
+{
+  food.remove();
+  score += 1;
 }
