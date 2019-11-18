e102af1e0c09f74c7ffd88e5283d3313886587e6
Git4JEOL <claener@naver.com>
2019-11-18 15:51:22 +0900

    Update sketch.js
    add random block and delete map

--- sketch.js
+++ sketch.js
@@ -2,23 +2,21 @@ var r,g,b; //random value
 function setup() {
   createCanvas(800, 400);
   player=createSprite(0,0,50,50);
-  box1=createSprite(200,100,200,300);
-  box2=createSprite(600,300,200,300);
   food = new Group();
   //food
    for (var i = 0; i < 10; i++) {
-     var prey =createSprite(random(0,width),random(0,height));
+     var prey =createSprite(random(0,width),random(0,height),60,60);
+     prey.shapeColor= color(0,100,100);
      food.add(prey);
    }
-   food.draw = function() { ellipse(0,0,10,10) }
-   //obstacles = new group();
-   /*for (var i = 0; i < 5; i++) {
-     var prey =createSprite(random(0,width),randim(0,height));
-     block.add(prey);
+   obstacles = new Group();
+   for (var i = 0; i < 10; i++) {
+     var block =createSprite(random(0,width),random(0,height),random(20,50),random(20,50));
+     block.shapeColor= color(255);
+     obstacles.add(block);
    }
-   */
-}
 
+}
 
 function draw() {
   background(100,200,300);
@@ -32,16 +30,17 @@ function draw() {
     player.shapeColor = color(r,g,b);//player color change random
   }
   player.overlap(food,eat);
-  if(player.overlap(box1)||player.overlap(box2)){//overlap
+if(player.overlap(obstacles)){//overlap
     player.remove();//remove if overlap
     food.remove();
     box2.remove();
     fill(255);
     rect(0,0,800,400);//game over white background
     textSize(50);
-    fill(100,200,300);
+    fill(255);
     text('Game over', 300, 200);
     noLoop();//stop game
+
   }
   drawSprites();
 }
