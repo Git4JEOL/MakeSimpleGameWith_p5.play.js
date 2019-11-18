d92fbb740711406e6694f54caffea309aa40413e
Git4JEOL <claener@naver.com>
2019-11-18 17:27:47 +0900

    Update sketch.js
    add position and physics value about box

--- sketch.js
+++ sketch.js
@@ -3,10 +3,21 @@ function setup() {
   createCanvas(800, 400);
   box = createSprite(width/2, height/2,
     40, 40);
-
+  box.shapeColor = color(255);
+  box.velocity.y = 0;
 }
-
 function draw() {
   background(0);
+  if (box.position.y >= height) {
+    box.velocity.y *= -1;
+    box.position.y = height;
+  }
+
+  box.addSpeed(0.2, 90);
+  drawSprites();
+
+}
+function mousePressed() {
+  box.position.y = mouseY;
 
 }
